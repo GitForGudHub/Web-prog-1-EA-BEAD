86b2013994f660e560f4a0eaa15b5881ead84945
GetGitGudHub <mateszixi@gmail.com>
2025-04-26 10:38:27 +0200

    third commit

--- Assets/JS/table.js
+++ Assets/JS/table.js
@@ -9,13 +9,13 @@ form.addEventListener("submit", function (e) {
   const name = document.getElementById("name").value.trim();
   const age = document.getElementById("age").value.trim();
   const city = document.getElementById("city").value.trim();
-  const job = document.getElementById("job").value.trim();
+  const mail = document.getElementById("mail").value.trim();
 
-  if (!name || !age || !city || !job) return alert("Minden mező kötelező!");
-  if (name.length < 2 || city.length < 2 || job.length < 2) return alert("Túl rövid adat!");
-  if (name.length > 30 || city.length > 30 || job.length > 30) return alert("Túl hosszú adat!");
+  if (!name || !age || !city || !mail) return alert("Minden mező kötelező!");
+  if (name.length < 2 || city.length < 2 || mail.length < 2) return alert("Túl rövid adat!");
+  if (name.length > 30 || city.length > 30 || mail.length > 30) return alert("Túl hosszú adat!");
 
-  const newRow = { name, age, city, job };
+  const newRow = { name, age, city, mail };
 
   if (editIndex === -1) {
     rows.push(newRow);
@@ -36,7 +36,7 @@ function renderTable(filteredRows = rows) {
       <td>${row.name}</td>
       <td>${row.age}</td>
       <td>${row.city}</td>
-      <td>${row.job}</td>
+      <td>${row.mail}</td>
       <td>
         <button onclick="editRow(${index})">✏️</button>
         <button onclick="deleteRow(${index})">🗑️</button>
@@ -50,7 +50,7 @@ function editRow(index) {
   document.getElementById("name").value = row.name;
   document.getElementById("age").value = row.age;
   document.getElementById("city").value = row.city;
-  document.getElementById("job").value = row.job;
+  document.getElementById("mail").value = row.mail;
   editIndex = index;
 }
 
